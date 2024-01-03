8e7ee0a84e632f2d82c4a2b70a6eb0189dbc2215
= <isaacnmartin51@gmail.com>
2024-01-03 17:14:40 -0500

    just make it work with razor pages for now.

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -16,16 +16,15 @@ public class LogGroup(DateOnly SmokedDate, int Count)
 public class IndexModel(StContext context) : PageModel
 {
     public IEnumerable<LogGroup> Logs { get; set; }
-    [BindProperty]
-    public Log CreateLog { get; set; }
     public async Task OnGetAsync()
     {
         Logs = await context.Logs.GroupBy(l => new DateOnly(l.DateSmoked.Year, l.DateSmoked.Month, l.DateSmoked.Day), l => l.Id ).Select(lg => new LogGroup(lg.Key, lg.Count())).ToListAsync();
     }
-    
+
     public async Task OnPostAsync()
     {
-        context.Add(CreateLog);
+        var log = new Log(Guid.NewGuid(), DateTime.Now);
+        context.Add(log);
         await context.SaveChangesAsync();
         Logs = await context.Logs.GroupBy(l => new DateOnly(l.DateSmoked.Year, l.DateSmoked.Month, l.DateSmoked.Day), l => l.Id ).Select(lg => new LogGroup(lg.Key, lg.Count())).ToListAsync();
     }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.EntityFrameworkCore;
 using SmokeTracker.Data;
 
@@ -5,24 +6,26 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddRazorComponents();
 builder.Services.AddDbContext<StContext>(opts => opts.UseInMemoryDatabase("SmokeTracker"));
 
 var app = builder.Build();
-    using (var scope = app.Services.CreateScope() )
+
+using (var scope = app.Services.CreateScope())
+{
+    var context = scope.ServiceProvider.GetService<StContext>();
+
+    var logs = new List<Log>();
+
+    for (int i = 0; i < 10; i++)
     {
-        var context = scope.ServiceProvider.GetService<StContext>();
-
-        var logs = new List<Log>();
-
-        for (int i = 0; i < 10; i++)
-        {
-            var log = new Log(Guid.NewGuid(), DateTime.Now.AddDays(i));
-            logs.Add(log);
-        }
-
-        await context.Logs.AddRangeAsync(logs);
-        await context.SaveChangesAsync();
+        var log = new Log(Guid.NewGuid(), DateTime.Now.AddDays(i));
+        logs.Add(log);
     }
+
+    await context.Logs.AddRangeAsync(logs);
+    await context.SaveChangesAsync();
+}
 
 
 // Configure the HTTP request pipeline.
@@ -31,7 +34,7 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
-    
+
 
 }
 
